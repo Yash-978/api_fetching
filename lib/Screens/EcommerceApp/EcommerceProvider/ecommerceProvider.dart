47c2c315a149fe4a70a03c4237bda720c1bf4e3d
Yash-978 <virwalyash@gmail.com>
2024-07-30 12:06:52 +0530

    Initial Commit

--- lib/Screens/EcommerceApp/EcommerceProvider/ecommerceProvider.dart
+++ lib/Screens/EcommerceApp/EcommerceProvider/ecommerceProvider.dart
@@ -2,15 +2,19 @@ import 'package:api_fetching/Screens/EcommerceApp/EcommerceModel/ecommerceModel.
 import 'package:api_fetching/Screens/EcommerceApp/EcommercreApiHelper/ecommerceApiHelper.dart';
 import 'package:flutter/cupertino.dart';
 
-class EcommerceProvider extends ChangeNotifier{
-  EcommerceApiHelper ecommerceApiHelper =EcommerceApiHelper();
+class EcommerceProvider extends ChangeNotifier {
+  EcommerceApiHelper ecommerceApiHelper = EcommerceApiHelper();
   EcommerceModel? ecommerceModel;
-  Future<EcommerceModel?>fromEcommerceApi()async{
+
+  Future<EcommerceModel?> fromEcommerceApi() async {
     final data = await ecommerceApiHelper.fetchEcommerceData();
-    ecommerceModel=EcommerceModel.fromJson(data);
-    print('--------------------------------------------------provider============================');
+    ecommerceModel = EcommerceModel.fromJson(data);
+    print(
+        '--------------------------------------------------provider============================');
     return ecommerceModel;
   }
-
-
 }
+
+
+
+
